4a0b28b20a0250a78fb6c5d71ad757451d6f278b
Dhvani Dumaniya <ddumaniya866@gmail.com>
2025-01-08 17:16:26 +0530

    handle cart and checkout page for the logged in user

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
+from itertools import product
 from django.contrib import messages,auth
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from accounts.models import Account
+from carts.models import Cart, CartItem
+
+from carts.views import _cart_id
+from carts.models import Cart,CartItem
+import requests
+
 from .forms import RegistrationForm
 from django.views.decorators.csrf import csrf_exempt
 
@@ -59,9 +66,70 @@ def login(request):
         
         user = auth.authenticate(email=email,password=password)
         if user is not None:
+            try:
+                # print('entering inside try block')
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exist = CartItem.objects.filter(cart=cart).exists()                
+                # print(is_cart_item_exist)
+                if is_cart_item_exist:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    # print(cart_item)
+                    # getting product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+                        
+                        # get teh cart items from the user to acceshis product variation
+                    cart_item = CartItem.objects.filter(user=user) #product=product
+                    ex_var_list = []
+                    id =  []
+                    for item in cart_item:
+                        existing_variation = item.variation.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+                    # for item in cart_item:
+                    #     item.user = user
+                    #     item.save()  
+                    
+                    # product_variation = [1,2,3,4,6]
+                    # ex_var_list = [4,6,3,5]
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                            # print("Product Variations:", product_variation)
+                            # print("Existing Variations:", ex_var_list)
+                            # print("IDs:", id)
+
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save() 
+            except:
+                # print('entering inside except block')
+                pass
             auth.login(request, user)
             messages.success(request,'you are now logged in.')
-            return redirect('dashboard')
+            #request library
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # print('quary -> ', query)
+                # print('------')
+                #next=/cart/checkout/
+                params = dict(x.split('=')for x in query.split('&'))
+                # print('params ->',params)
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request,'Invalid login credentials')
             return redirect('login')
